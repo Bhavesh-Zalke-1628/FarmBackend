3c13c1fadca327b6dd9859999b59c91e8905074b
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-06-24 11:52:15 +0530

    basic changes

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ import { config } from "dotenv"
 config()
 
 const app = express()
-app.use(cors({
-    origin: process.env.FRONTEND_URI,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URI,
+        methods: ["POST", "GET", "PUT", "PUT", "PATCH", "DELETE"],
+        credentials: true,
+    })
+);
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
