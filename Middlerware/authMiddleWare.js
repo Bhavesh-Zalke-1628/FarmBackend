e78f09a604b5c460d5fa8c8f12d0c2a6efd5bd27
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-06-26 23:51:25 +0530

    changes

--- Middlerware/authMiddleWare.js
+++ Middlerware/authMiddleWare.js
@@ -4,9 +4,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken'
 
-import jwt from "jsonwebtoken";
-import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model.js";
 
 const isLoggedIn = async (req, res, next) => {
     try {
